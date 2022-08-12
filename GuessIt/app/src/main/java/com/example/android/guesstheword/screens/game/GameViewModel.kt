b3db5196929474aad17fea025b962e2618a0a9bf
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-08-12 13:42:35 +0200

    Updated Score & word to liveData

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,23 +1,26 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init{
-        Log.i("GameViewModel", "ViewModel Created")
         resetList()
         nextWord()
+
+        score.value = 0
+        word.value = ""
     }
 
 //    OnCleared gets called when the activity or fragment the viewModel
@@ -28,12 +31,12 @@ class GameViewModel : ViewModel() {
     }
 
     fun onSkip() {
-        score--
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = (score.value)?.plus(1)
         nextWord()
     }
 
@@ -45,7 +48,7 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
 //            gameFinished()
         } else {
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
 
     }
