1924a90e00b2a210b2ad11e0aa1f87231807c266
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-08-13 00:30:06 +0200

    added Databinding to liveData

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -61,14 +61,8 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         binding.gameViewModel = viewModel
+        binding.lifecycleOwner = this
 
-        viewModel.score.observe(this, Observer { newScore->
-            binding.scoreText.text = newScore.toString()
-        })
-
-        viewModel.word.observe(this, Observer { newWord->
-            binding.wordText.text = newWord
-        })
 
         viewModel.eventGameFinish.observe(this, Observer{ isFinished ->
             if(isFinished) {
