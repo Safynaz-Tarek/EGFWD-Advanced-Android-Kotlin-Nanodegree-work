9d971e28e5b2ad61102329447f965baf79a878f8
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-08-21 01:37:07 +0200

    added SleepQuality

--- sleepTracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ sleepTracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -19,10 +19,7 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.app.Application
 import android.provider.SyncStateContract.Helpers.insert
 import android.util.Log
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.*
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.formatNights
@@ -39,6 +36,14 @@ class SleepTrackerViewModel(
         private var tonight = MutableLiveData<SleepNight?>()
 
         private val nights = database.getAllNight()
+
+        private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+        val navigateToSleepQuality : LiveData<SleepNight>
+                get() = _navigateToSleepQuality
+
+        fun doneNavigating(){
+                _navigateToSleepQuality.value = null
+        }
 
         init {
             initializeTonight()
@@ -76,6 +81,7 @@ class SleepTrackerViewModel(
                         val oldNight = tonight.value ?: return@launch
                         oldNight.endTimeMilli = System.currentTimeMillis()
                         update(oldNight)
+                        _navigateToSleepQuality.value = oldNight
                 }
         }
 
