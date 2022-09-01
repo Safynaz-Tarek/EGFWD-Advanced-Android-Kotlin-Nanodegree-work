a16b2b68d5681cd007830daba77ee5b1421d9ddd
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-09-01 23:57:04 +0200

    Created Retrofit service, returns json response

--- marsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ marsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,37 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+//Retrofit is a library that creates a network API for our APP
+//Based on the content from our web service
+//It fetches data from our web service and route it through a separate converter library
+//That knows how to decode data and return it in the form of useful objects
 private const val BASE_URL = "https://mars.udacity.com/"
+
+
+//Scalar converter supports returning strings and other primitive types
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService{
+//    Retrofit append the endpoint realestate the base url and creates a call object
+//    The call object is used to start the request
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+
+// The create call is expensive and our app needs only one service instance we implement
+// it using a public object, call this object will return a retrofit object that implement MarsAPIservice
+object MarsApi{
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
--- marsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ marsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,6 +20,11 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -44,6 +49,19 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+//        the getProperties returns a call object, we then call enqeue on that callback
+//        starting a network request on a background thread
+//        the enqueue method takes a retrofit callback classes input that contains method to be called
+//        when the request is complete
+        MarsApi.retrofitService.getProperties().enqueue(object: Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure" + t.message
+            }
+
+        })
     }
 }
