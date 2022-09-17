1177b1507f4af8d03fa2a2cf69152e453a8dc822
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-09-17 22:07:48 +0200

    added Data messages

--- eggTimer/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ eggTimer/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,6 +1,9 @@
 package com.example.android.eggtimernotifications
 
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -10,6 +13,26 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "From ${remoteMessage?.from}")
+
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message data payload: "+ remoteMessage.data)
+        }
+
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification body:  ${it.body}")
+            sendNotification(it.body!!)
+        }
+
+    }
+
+    private fun sendNotification(messageBody: String){
+        val notificationManger = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManger.sendNotification(messageBody, applicationContext)
 
     }
 
