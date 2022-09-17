db15e4838308e022225fd5a70a40f4567ac1c0b1
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-09-18 00:03:40 +0200

    added frame

--- MiniPaint/app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ MiniPaint/app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -1,10 +1,7 @@
 package com.example.minipaint
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -43,6 +40,8 @@ class MyCanvasView(context: Context): View(context) {
 
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
+    private lateinit var frame: Rect
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
@@ -50,13 +49,16 @@ class MyCanvasView(context: Context): View(context) {
 
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
-
         extraCanvas.drawColor(backgroundColor)
+
+        val inset = 40
+        frame = Rect(inset, inset, width-inset, height -inset)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas.drawRect(frame,paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
