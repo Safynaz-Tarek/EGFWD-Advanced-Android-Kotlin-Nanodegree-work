ca98258d6a98b19512c17aced135b06b62158e1e
Safynaz-Tarek <48590985+Safynaz-Tarek@users.noreply.github.com>
2022-10-05 23:32:17 +0200

    added login & logout

--- android-kotlin-login-start/app/src/main/java/com/example/android/firebaseui_login_sample/LoginViewModel.kt
+++ android-kotlin-login-start/app/src/main/java/com/example/android/firebaseui_login_sample/LoginViewModel.kt
@@ -20,6 +20,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
 import androidx.preference.PreferenceManager
+import com.google.firebase.auth.FirebaseUser
 import kotlin.random.Random
 
 class LoginViewModel : ViewModel() {
@@ -42,6 +43,15 @@ class LoginViewModel : ViewModel() {
 
     enum class AuthenticationState {
         AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+
+    }
+
+    val  authenticationState = FirebaseUserLiveData().map{    user ->
+        if( user != null){
+        AuthenticationState.AUTHENTICATED
+        }else{
+            AuthenticationState.UNAUTHENTICATED
+        }
     }
 
     // TODO Create an authenticationState variable based off the FirebaseUserLiveData object. By
